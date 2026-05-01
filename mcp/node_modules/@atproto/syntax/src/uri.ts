7ec0815fe78a15c3656f6be0cf6625d5d116d5f9
xmrtnet-eliza <xmrtnet@gmail.com>
2026-04-30 18:24:24 -0600

    Add .gitignore for node_modules, remove from tracking

--- mcp/node_modules/@atproto/syntax/src/uri.ts
+++
@@ -1,5 +0,0 @@
-export type UriString = `${string}:${string}`
-
-export function isValidUri<I extends string>(input: I): input is I & UriString {
-  return /^\w+:(?:\/\/)?[^\s/][^\s]*$/.test(input)
-}
